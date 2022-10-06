b544228bab60e8a688f0afd77c1fa5fa306860b1
Leyci Buelvas <leycibuelvas73@gmail.com>
2022-10-05 22:33:35 -0500

    challenge complete

---
+++ src/dao/archivos/Carts.js
@@ -0,0 +1,11 @@
+import archivosContenedor from "./archivosContenedor.js";
+import __dirname from "../../utils.js";
+
+const archivosCarts = __dirname + "/files/carts.txt";
+
+export default class Carts extends archivosContenedor {
+    constructor() {
+        super(archivosCarts);
+    }
+};
+
---
+++ src/dao/mongo/Carts.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"; 
+import MongoDBContainer from "./MongoDBContainer.js";
+
+const collection = 'carts';
+
+const productosSchema = mongoose.Schema({
+    productos: [
+        {
+            producto: {
+                type: mongoose.SchemaTypes.ObjectId,
+                ref: 'productos'
+            },
+            quantity: Number
+        }
+    ]
+}, {timestamps: true});
+
+export default class Carts extends MongoDBContainer {
+    constructor() {
+        super(collection, productosSchema);
+    }
+}
+
